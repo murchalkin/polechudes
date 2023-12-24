56ef2b5682ac53e817fa56a3117fb3c8bae5d27d
murchalkin <106840199+murchalkin@users.noreply.github.com>
2023-12-25 00:06:09 +0300

    Make winner data

--- keepalive.php
+++ keepalive.php
@@ -3,6 +3,11 @@ include 'config.php';
 use configuration\config;
 $yourid = $_GET['id'];
 $context = file_get_contents("users.txt");
+$winner = file_get_contents("game/winned.txt");
+if($context == ""){
+  echo $winner;
+  return;
+}
 if(in_array($yourid, explode(',', $context))){
 $showedwords = file_get_contents("game/unhidded.txt");
 echo $showedwords;
