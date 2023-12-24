078b4ed7e68b843c3a302836ad5b01f65709c173
murchalkin <106840199+murchalkin@users.noreply.github.com>
2023-12-25 00:08:53 +0300

    Improvement code

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ class config{
   public static $clientsstart = 2; // clients on server for start, set -1, if dont allow connect
   
   // Auth
-  public static $token = "пароль"; // token auth
+  public static $token = "мырон"; // token auth
   public static $minrandomid = -100; // minimum random generating id
   public static $maxrandomid = 1; // maximum random generating id(max length:19), dont use negative number or 0
   public static $codeerrorauth = "Ошибка авторизации"; // error auth code returning to client
@@ -15,7 +15,7 @@ class config{
   public static $codesuccessauth = ""; // success auth code returning to client
 
   // Game
-  public static $words = "мышонок,табуретка,кошка,игра"; // words generating(dont use spaces)
+  public static $words = "шмыгыч,циркачи,выебоны,чмарина"; // words generating(dont use spaces, dont use numbers)
   public static $codeerrordontstarted = "Игра не начата"; // error game not started code returning to client
   public static $codeerrordontauthorized = "Вы не авторизованы"; // code error when dont authorized
   public static $codedontqueue = "Не ваша очередь называть букву"; // code dont you queue returning to client
@@ -24,7 +24,6 @@ class config{
   public static $codeexistsletter = "Такую букву уже угадали"; // letter already exists code returning to client
   public static $codeerrorlengthletter = "Буква не может содержать больше двух букв"; // error when length letter more than 2 code returning to client
   public static $codeyouwin = "Ты выиграл"; // code when player is win returning to client
-  public static $codewin = "Выиграл игрок с ником:"; // code when player is win returning to all clients
   
 }
 }
