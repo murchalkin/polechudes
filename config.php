79dfc5efae3392a534fab1dddb0ffdac22720267
murchalkin <106840199+murchalkin@users.noreply.github.com>
2023-12-25 17:39:10 +0300

    Updated notions, and words

--- config.php
+++ config.php
@@ -12,13 +12,11 @@ class config{
   public static $maxrandomid = 1; // maximum random generating id(max length:19), dont use negative number or 0
   public static $codeerrorauth = "Ошибка авторизации"; // error auth code returning to client
   public static $codeerrorfull = "Сервер переполнен"; // error id or number clients full code returning to client
-  public static $codesuccessauth = ""; // success auth code returning to client
 
   // Game
-  public static $words = "стол,кошка,табуретка"; // words generating(dont use spaces, dont use numbers)
+  public static $words = "табуретка,пряник,торт,шаурма,бургер,кошка,хотдог"; // words generating(dont use spaces, dont use numbers)
   public static $codeerrordontstarted = "Игра не начата"; // error game not started code returning to client
   public static $codeerrordontauthorized = "Вы не авторизованы"; // code error when dont authorized
-  public static $codedontqueue = "Не ваша очередь называть букву"; // code dont you queue returning to client
   public static $codesuccessletter = "Буква правильная"; // success send letter code returning to client
   public static $codewrongletter = "Буква неверная"; // wrong letter code returning to client
   public static $codeexistsletter = "Такую букву уже угадали"; // letter already exists code returning to client
