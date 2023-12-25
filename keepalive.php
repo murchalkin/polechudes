0b7b8627b4e61ee26908c3c7cab6aaf58baa38a1
murchalkin <106840199+murchalkin@users.noreply.github.com>
2023-12-25 17:40:04 +0300

    Bug fix

--- keepalive.php
+++ keepalive.php
@@ -5,7 +5,13 @@ $yourid = $_GET['id'];
 $context = file_get_contents("users.txt");
 $winner = file_get_contents("game/winned.txt");
 if($context == ""){
+  if($winner != ""){
   echo $winner;
+  return;
+  }
+}
+if(file_get_contents("game/gamestarted.txt") == ""){
+  echo config::$codeerrordontstarted;
   return;
 }
 if(in_array($yourid, explode(',', $context))){
