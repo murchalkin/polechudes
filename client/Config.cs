17cb70805979f7bcad9a1a56c1229ea6ec5eebb3
murchalkin <106840199+murchalkin@users.noreply.github.com>
2023-12-26 16:48:13 +0300

    Deleted excess

--- client/Config.cs
+++ client/Config.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Config : MonoBehaviour
 {
      // Global
-     public static string mainurl = "https://mushypreciousapplicationstack.nikitychkitych.repl.co"; // main url server
+     public static string mainurl = ""; // main url https server
      public static string tokenauth = "vsiJrFivtbMlnxOH8FYBeQ=="; // hashed token for auth on server
      public static float keepalivetime = 0.5f; // keep alive time (seconds, allowed digits)
 }
