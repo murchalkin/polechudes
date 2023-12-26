924357b91a070d9f7ed9f73c75bdeca342004bdb
murchalkin <106840199+murchalkin@users.noreply.github.com>
2023-12-26 20:39:16 +0300

    Added hashing ids

--- keepalive.php
+++ keepalive.php
@@ -2,7 +2,7 @@
 include 'config.php';
 use configuration\config;
 $yourid = $_GET['id'];
-$context = file_get_contents("users.txt");
+$context = file_get_contents("game/users.txt");
 $winner = file_get_contents("game/winned.txt");
 if($context == ""){
   if($winner != ""){
@@ -14,7 +14,7 @@ if(file_get_contents("game/gamestarted.txt") == ""){
   echo config::$codeerrordontstarted;
   return;
 }
-if(in_array($yourid, explode(',', $context))){
+if(in_array(openssl_encrypt($yourid, "AES-128-ECB", config::$passwordcipher), explode(',', $context))){
 $showedwords = file_get_contents("game/unhidded.txt");
 $arraykeepalive = explode(',', file_get_contents("game/countkeepalives.txt"));
 $indexik = array_search($yourid, explode(',', $context));
