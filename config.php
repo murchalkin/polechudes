d7d0f5760cd765fea2cb5f7ddfe5aae68f2fe948
murchalkin <106840199+murchalkin@users.noreply.github.com>
2023-12-26 16:40:19 +0300

    Added new config

--- config.php
+++ config.php
@@ -4,14 +4,15 @@ class config{
   // Global
   public static $passwordcipher = "password"; // password cipher
   public static $codeerrorserver = "Ошибка сервера"; // error server code returning to client
-  public static $clientsstart = 2; // clients on server for start, set -1, if dont allow connect
+  public static $clientsstart = 3; // clients on server for start, set -1, if dont allow connect
   
   // Auth
   public static $token = "мырон"; // token auth
-  public static $minrandomid = -100; // minimum random generating id
+  public static $minrandomid = -1000; // minimum random generating id
   public static $maxrandomid = 1; // maximum random generating id(max length:19), dont use negative number or 0
   public static $codeerrorauth = "Ошибка авторизации"; // error auth code returning to client
   public static $codeerrorfull = "Сервер переполнен"; // error id or number clients full code returning to client
+  public static $minkeepalivesneed = 100; // minimum keep alives need every minute, if less the value, then kick client(checking inactivity clients, happens when new user want connect on full server)
 
   // Game
   public static $words = "табуретка,пряник,торт,шаурма,бургер,кошка,хотдог"; // words generating(dont use spaces, dont use numbers)
