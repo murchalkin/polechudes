7bfb88c842ca521a2892e4ce597805c29ca26a08
murchalkin <106840199+murchalkin@users.noreply.github.com>
2024-05-21 17:36:15 +0300

    Major update

--- keepalive.php
+++ keepalive.php
@@ -1,6 +1,13 @@
 <?php
 include 'config.php';
 use configuration\config;
+if(config::$OptimizeForJs == True){
+header('Access-Control-Allow-Origin: *'); 
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+}
 $yourid = $_GET['id'];
 $context = file_get_contents("game/users.txt");
 $winner = file_get_contents("game/winned.txt");
@@ -17,7 +24,7 @@ if(file_get_contents("game/gamestarted.txt") == ""){
 if(in_array(openssl_encrypt($yourid, "AES-128-ECB", config::$passwordcipher), explode(',', $context))){
 $showedwords = file_get_contents("game/unhidded.txt");
 $arraykeepalive = explode(',', file_get_contents("game/countkeepalives.txt"));
-$indexik = array_search($yourid, explode(',', $context));
+$indexik = array_search(openssl_encrypt($yourid, "AES-128-ECB", config::$passwordcipher), explode(',', $context));
 $arraykeepalive[$indexik] = intval($arraykeepalive[$indexik]) + 1;
 file_put_contents("game/countkeepalives.txt", implode(',', $arraykeepalive));
 echo $showedwords;
