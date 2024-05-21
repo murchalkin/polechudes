b413efd9879479e7f814ab68e52c46768dbde243
murchalkin <106840199+murchalkin@users.noreply.github.com>
2024-05-21 17:45:47 +0300

    Major update

--- client/SendLetter.cs
+++ client/SendLetter.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 
 public class SendLetter : MonoBehaviour
 {
-    public InputField inputfieldletter;
-    public Text log;
+    public GameObject inputfieldletter;
+    public TextMeshProUGUI log;
 
     public void SendLetterr(){
-       if(inputfieldletter.text == "") return;
+       if(inputfieldletter.GetComponent<TMP_InputField>().text == "") return;
        StartCoroutine(Auther());
     }
     IEnumerator Auther(){
-       UnityWebRequest request = UnityWebRequest.Get($"{Config.mainurl}/sendletter.php?id={Auth.myid}&letter={inputfieldletter.text}");
+       UnityWebRequest request = UnityWebRequest.Get($"{Config.mainurl}/sendletter.php?id={Auth.myid}&letter={inputfieldletter.GetComponent<TMP_InputField>().text}");
        yield return request.SendWebRequest();
        log.text = request.downloadHandler.text;
        
     }
 }
+
